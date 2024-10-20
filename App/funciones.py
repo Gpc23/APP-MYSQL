f2a2e62f8b346c3964390d033d37164c4bd627f4
Gonzalo Peña Calero <pcg.200204@gmail.com>
2024-10-20 12:16:55 +0200

    Añadido aplicacion

---
+++ App/funciones.py
@@ -0,0 +1,19 @@
+import sys
+import mysql.connector
+
+def Conectar_BD(user, password, database):
+    try:
+        db = mysql.connector.connect(
+            host="192.168.122.158",
+            user=user,
+            password=password,
+            database=database
+        )
+    except mysql.connector.Error as e:
+        print("No se pudo conectar a la base de datos:", e)
+        sys.exit(1)
+    return db
+
+
+def Desconectar_BD(db):
+    db.close()
