f724b67e11ce2367921a3f998492d20eb6a20673
Gonzalo Peña Calero <pcg.200204@gmail.com>
2024-10-20 12:41:14 +0200

    Modificado entorno y aplicacion

--- App/venv/bin/activate.fish
+++ App/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gonzalopc/git/app-mysql/App/venv"
+set -gx VIRTUAL_ENV "/home/gonzalopc/git/APP-MYSQL/App/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
